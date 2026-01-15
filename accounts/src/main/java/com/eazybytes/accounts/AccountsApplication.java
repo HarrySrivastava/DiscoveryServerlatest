c5dda049965b58e15d19ff704dafdfd2bebdb973
HarrySrivastava <haripesit@gmail.com>
2026-01-15 15:25:04 +0530

    Initial commit

---
+++ accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,41 @@
+package com.eazybytes.accounts;
+
+import com.eazybytes.accounts.dto.AccountsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@EnableJpaAuditing(auditorAwareRef="auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Hari Srivastava",
+						email = "tutor@eazybytes.com",
+						url = "https://www.eazybytes.com"
+				)),
+		externalDocs = @ExternalDocumentation(
+				description =  "EazyBank Accounts microservice REST API Documentation",
+				url = "https://www.eazybytes.com/swagger-ui.html"
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
---
+++ loans/src/main/java/com/eazybytes/loans/dto/LoansContactInfoDto.java
@@ -0,0 +1,22 @@
+package com.eazybytes.loans.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.Map;
+@ConfigurationProperties(prefix = "loans")
+@Getter
+@Setter
+/*
+public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+}
+
+ */
+public class LoansContactInfoDto
+{
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+}
